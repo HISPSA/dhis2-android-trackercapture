0caf3c2dc6232ea1c3b7aae09c3fa39d1d3f2361
TAG <arthur.gwatidzo@gmail.com>
2016-03-16 12:44:09 +0200

    Disable Complete and Terminate Buttons in the ProgramOverviewFragment

--- app/src/main/java/org/hisp/dhis/android/trackercapture/fragments/programoverview/ProgramOverviewFragment.java
+++ app/src/main/java/org/hisp/dhis/android/trackercapture/fragments/programoverview/ProgramOverviewFragment.java
@@ -302,8 +302,11 @@ public class ProgramOverviewFragment extends AbsProgramRuleFragment implements V
         terminateButton = (Button) header.findViewById(R.id.terminate);
         followupButton = (ImageButton) header.findViewById(R.id.followupButton);
         profileButton = (ImageButton) header.findViewById(R.id.profile_button);
-        completeButton.setOnClickListener(this);
-        terminateButton.setOnClickListener(this);
+
+        completeButton.setEnabled(false);
+        terminateButton.setEnabled(false);
+        //completeButton.setOnClickListener(this);
+        //terminateButton.setOnClickListener(this);
         followupButton.setOnClickListener(this);
         followupButton.setVisibility(View.GONE);
         profileButton.setOnClickListener(this);
